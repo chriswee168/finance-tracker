14e91f7958a4ae70867b1fc85193abf44e4b61dd
Christopher Wee <chriswee168@gmail.com>
2026-06-19 21:59:03 +0800

    Create add_net_income route
    Adds entry to net_income_table

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routers import test, create_table
+from routers import test, create_table, add_net_income
 
 app = FastAPI()
 
 app.include_router(test.route)
 app.include_router(create_table.route)
+app.include_router(add_net_income.route)
 
 @app.get("/helloworld")
 def helloworld():
