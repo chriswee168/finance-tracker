37c1734cf1733277cd2e189a2cbff878231ab1f9
Christopher Wee <chriswee168@gmail.com>
2026-06-25 10:37:48 +0800

    Add rule to calculate and return net income

--- backend/routers/net_income.py
+++ backend/routers/net_income.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 import sqlite3
 import os
 
 route = APIRouter()
+
+class NetIncomeResponse(BaseModel):
+    net_income_cents: int
 
 # Add to net income SQL table.
 @route.post("/add-net-income")
@@ -18,3 +22,24 @@ def add_net_income(entry_id: int, entry_date: str, ammount: str):
     cursor.execute(add_net_income_query)
     conn.commit()
     conn.close()
+
+# Calculate the net income from the income and expenses SQL table.
+@route.get("/calc-net-income", response_model=NetIncomeResponse)
+def calc_net_income():
+    conn = sqlite3.connect("finance_database.sqlite3")
+    cursor = conn.cursor()
+
+    obtain_income_sum_query = "SELECT SUM(amount) FROM net_income_table WHERE category_type = 'income'"
+    obtain_expenses_sum_query = "SELECT SUM(amount) FROM net_income_table WHERE category_type = 'expense'"
+
+    # Get income and expense sum.
+    cursor.execute(obtain_income_sum_query)
+    income_sum = cursor.fetchall()
+    cursor.execute(obtain_expenses_sum_query)
+    expenses_sum = cursor.fetchall()
+    conn.close()
+
+    # Net income = total income - total expenses
+    net_income = income_sum - expenses_sum
+
+    return {"net_income": net_income}
