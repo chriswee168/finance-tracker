6a6673353e9b4777db2330ea504622eceb358363
Christopher Wee <chriswee168@gmail.com>
2026-06-25 12:13:57 +0800

    Rename add_category.py to category.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from routers import net_income, test, create_table
-from routers import add_category
+from routers import category
 
 app = FastAPI()
 
 app.include_router(test.route)
 app.include_router(create_table.route)
 app.include_router(net_income.route)
-app.include_router(add_category.route)
+app.include_router(category.route)
 
 @app.get("/helloworld")
 def helloworld():
