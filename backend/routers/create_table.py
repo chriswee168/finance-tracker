e0cd865b30d4bc4a08933e1d759060c23b6ef7e7
Christopher Wee <chriswee168@gmail.com>
2026-06-25 18:16:16 +0800

    Use "IF NOT EXISTS" to avoid creating existing tables

--- backend/routers/create_table.py
+++ backend/routers/create_table.py
@@ -7,38 +7,37 @@ route = APIRouter()
 # Create SQL table to record net income overtime.
 @route.patch("/create-tables")
 def create_tables():
-    if not os.path.exists("finance_database.sqlite3"):
-        conn = sqlite3.connect("finance_database.sqlite3")
-        cursor = conn.cursor()
+    conn = sqlite3.connect("finance_database.sqlite3")
+    cursor = conn.cursor()
         
-        create_net_income_query = (
-            "CREATE TABLE net_income_table(" \
-            "   entry_date DATE PRIMARY KEY," \
-            "   amount_cents INT," \
-            ")"
-        )
+    create_net_income_query = (
+        "CREATE TABLE IF NOT EXISTS net_income_table(" \
+        "   entry_date DATE PRIMARY KEY," \
+        "   amount_cents INT" \
+        ")"
+    )
 
-        create_category_table_query = (
-            "CREATE TABLE category_table(" \
-            "   category VARCHAR(100) PRIMARY KEY," \
-            "   category_type CHAR(7)" \
-            ")"
-        )
+    create_category_table_query = (
+        "CREATE TABLE IF NOT EXISTS category_table(" \
+        "   category VARCHAR(100) PRIMARY KEY," \
+        "   category_type CHAR(7)" \
+        ")"
+    )
 
-        create_income_expense_table_query = (
-            "CREATE TABLE income_expense_table(" \
-            "   entry_id INT PRIMARY KEY," \
-            "   entry_date DATE,"
-            "   category VARCHAR(100),"
-            "   category_type CHAR(7)" \
-            "   amount_cents INT," \
-            
-            "   FOREIGN KEY (category) REFERENCES category_table(category) ON DELETE CASCADE" \
-            ")"
-        )
+    create_income_expense_table_query = (
+        "CREATE TABLE IF NOT EXISTS income_expense_table(" \
+        "   entry_id INT PRIMARY KEY," \
+        "   entry_date DATE,"
+        "   category VARCHAR(100),"
+        "   category_type CHAR(7)," \
+        "   amount_cents INT," \
+        
+        "   FOREIGN KEY (category) REFERENCES category_table(category) ON DELETE CASCADE" \
+        ")"
+    )
 
-        cursor.execute(create_net_income_query)
-        cursor.execute(create_category_table_query)
-        cursor.execute(create_income_expense_table_query)
-        conn.commit()
-        conn.close()
+    cursor.execute(create_net_income_query)
+    cursor.execute(create_category_table_query)
+    cursor.execute(create_income_expense_table_query)
+    conn.commit()
+    conn.close()
