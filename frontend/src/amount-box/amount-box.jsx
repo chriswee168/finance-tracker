76874f8fdd9a4cdd3908629f629261f374dc5cb1
Christopher Wee <chriswee168@gmail.com>
2026-07-01 12:06:51 +0800

    Add sign and colour parameters
    Allow control of the sign and its colour to indicate an increase
    or decrease.

--- frontend/src/amount-box/amount-box.jsx
+++ frontend/src/amount-box/amount-box.jsx
@@ -6,15 +6,20 @@ import styles from "./amount-box.module.css";
  * @param {Object} param0 
  * @param {string} param0.textLabel Text to display above dollar amount.
  * @param {string} param0.amountDollars Amount in dollars.
+ * @param {string} param0.sign Positive or negative sign to indicate amount increase or decrease.
+ * @param {string} param0.colour Colour of positive/negative sign.
  * 
  * @returns Amount box component.
  */
-export default function AmountBox({textLabel, amountDollars})
+export default function AmountBox({textLabel, amountDollars, sign, colour})
 {
   return (
     <div className={styles.amountBox}>
       <p className={styles.textLabel}>{textLabel}</p>
-      <p className={styles.amountDollars}>{amountDollars}</p>
+      <p className={styles.amountDollars}>
+        <span style={{color: colour}}>{sign}</span>
+        ${amountDollars}
+      </p>
     </div>
   )
 }
