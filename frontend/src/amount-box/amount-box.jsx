03dd39a2183ce6023a8e1acf91c4285eb28b6eeb
Christopher Wee <chriswee168@gmail.com>
2026-07-01 11:24:15 +0800

    Add textLabel to amount box
    Indicate what the cash amount represents.

--- frontend/src/amount-box/amount-box.jsx
+++ frontend/src/amount-box/amount-box.jsx
@@ -4,15 +4,17 @@ import styles from "./amount-box.module.css";
 /**
  * Create the amount box component to display a cash amount.
  * @param {Object} param0 
- * @param {string} param0.amountDollars Amount in cents.
+ * @param {string} param0.textLabel Text to display above dollar amount.
+ * @param {string} param0.amountDollars Amount in dollars.
  * 
  * @returns Amount box component.
  */
-export default function AmountBox({amountDollars})
+export default function AmountBox({textLabel, amountDollars})
 {
   return (
     <div className={styles.amountBox}>
-      <p>{amountDollars}</p>
+      <p className={styles.textLabel}>{textLabel}</p>
+      <p className={styles.amountDollars}>{amountDollars}</p>
     </div>
   )
 }
