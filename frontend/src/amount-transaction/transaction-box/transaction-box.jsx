40b2995f95d4ca753a76acbefd9aebded6625522
Christopher Wee <chriswee168@gmail.com>
2026-07-06 21:53:55 +0800

    Add net income and net balance state parameters

--- frontend/src/amount-transaction/transaction-box/transaction-box.jsx
+++ frontend/src/amount-transaction/transaction-box/transaction-box.jsx
@@ -9,10 +9,17 @@ import styles from "./transaction-box.module.css";
  * @param {string} param0.transactionDesc Transaction description state.
  * @param {Dispatch<SetStateAction<string>>} param0.setAmount Setter for cash amount.
  * @param {Dispatch<SetStateAction<string>>} param0.setTransactionDesc Setter for transaction description.
+ * @param {string} param0.netIncomeStates State settings for net income.
+ * @param {string} param0.netBalanceStates State settings for net balance.
+ * @param {Dispatch<SetStateAction<string>>} param0.setNetIncomeStates Setter for net income.
+ * @param {Dispatch<SetStateAction<string>>} param0.setNetBalanceStates Setter for net balance.
  * 
  * @returns Transaction box component.
  */
-export default function TransactionBox({cashAmount, transactionDesc, setAmount, setTransactionDesc})
+export default function TransactionBox({
+  cashAmount, transactionDesc, setAmount, setTransactionDesc,
+  netIncomeStates, netBalanceStates, setNetIncomeStates, setNetBalanceStates
+})
 {
   return (
     <div className={styles.transactionBox}>
