b922753f6cdbb2a5f452330939a5ea6392eb40cf
Christopher Wee <chriswee168@gmail.com>
2026-07-10 12:02:17 +0800

    Remove category column

--- backend/routers/create_table.py
+++ backend/routers/create_table.py
@@ -27,11 +27,8 @@ def create_tables():
         "CREATE TABLE IF NOT EXISTS transaction_table(" \
         "   entry_id INT PRIMARY KEY," \
         "   entry_date DATE,"
-        "   category VARCHAR(100),"
         "   transaction_type CHAR(7)," \
         "   amount_cents INT," \
-        
-        "   FOREIGN KEY (category) REFERENCES category_table(category) ON DELETE CASCADE" \
         ")"
     )
 
