c2479a6bfbb114ebf9511f03df4c46d8e1688799
Christopher Wee <chriswee168@gmail.com>
2026-07-11 12:09:23 +0800

    Rename net_income.py to cash_amounts.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routers import net_income, create_table
+from routers import cash_amounts, create_table
 from routers import category
 
 app = FastAPI()
 
 # Link all routers.
 app.include_router(create_table.route)
-app.include_router(net_income.route)
+app.include_router(cash_amounts.route)
 app.include_router(category.route)
