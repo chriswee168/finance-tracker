d33d2c04d19339ba2804c477e37487ee7eaf6dc3
Christopher Wee <chriswee168@gmail.com>
2026-07-14 12:41:35 +0800

    Create the default message if empty

--- frontend/src/transaction-history/transaction-history.jsx
+++ frontend/src/transaction-history/transaction-history.jsx
@@ -8,6 +8,9 @@ import styles from "./transaction-history.module.css";
  */
 export default function TransactionHistory()
 {
+  // Default message to display when no transactions have ever been made.
+  const defaultMsg = <div className={styles.defaultMsg}>NO TRANSACTIONS</div>;
+
   // Array to store transaction entry components that include
   // details of previous transactions.
   const transactionEntries = [];
@@ -15,7 +18,9 @@ export default function TransactionHistory()
   return (
     <div className={styles.transactionHistory}>
       <h1>TRANSACTION HISTORY</h1>
-      <div className={styles.transactionList}>{transactionEntries}</div>
+      <div className={styles.transactionList}>
+        {transactionEntries.length == 0 ? defaultMsg : transactionEntries}
+      </div>
     </div>
   )
 }
