154b269e75d71f1c3b439112dddb2122e10d7539
Christopher Wee <chriswee168@gmail.com>
2026-07-14 19:47:24 +0800

    Move description into wrapper

--- frontend/src/transaction-history/transaction-entry/transaction-entry.jsx
+++ frontend/src/transaction-history/transaction-entry/transaction-entry.jsx
@@ -13,7 +13,9 @@ export default function TransactionEntry({datetime, type, amountDollars, desc})
     <div className={styles.transactionEntry}>
       <div className={styles.amountDollars}>{amountDollars}</div>
       <div className={styles.datetime}>{datetime}</div>
-      <div className={styles.desc}>{desc}</div>
+      <div className={styles.descWrapper}>
+        <div className={styles.desc}>{desc}</div>
+      </div>
     </div>
   )
 }
