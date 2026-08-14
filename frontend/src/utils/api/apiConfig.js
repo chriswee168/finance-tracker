c899ed8073abbf872b249ece9991df312235fb30
Christopher Wee <chriswee168@gmail.com>
2026-08-14 18:09:46 +0800

    Add VITE_API_URL env variable
    Allows React frontend to use URL of Render web service for FastAPI
    as an environment variable.

--- frontend/src/utils/api/apiConfig.js
+++ frontend/src/utils/api/apiConfig.js
@@ -1,6 +1,6 @@
 // Request URL configuration for FastAPI.
 const PORT = 8000;
-export const BASE_URL = `http://127.0.0.1:${PORT}`;
+export const BASE_URL = import.meta.env.VITE_API_URL || `http://127.0.0.1:${PORT}`;
 export const REQUEST_URLS = {
     TRANSACTIONS: `${BASE_URL}/transaction-entries`,
     CURRENT_AMOUNTS: `${BASE_URL}/current-cash-amounts`,
