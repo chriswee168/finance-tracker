f79aee0449323b94448e88f7465b9e73fe81d1d5
Christopher Wee <chriswee168@gmail.com>
2026-08-30 13:05:06 +0800

    Modify add_cash_amount method
    add_cash_amount method is modified to update the cash amounts
    in latest amount history entry.

--- backend/routers/cash_amounts.py
+++ backend/routers/cash_amounts.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, status, Response
 from pydantic import BaseModel
 import sqlite3, json
 from utils.constants import *
+from utils.amount_history_funcs import set_amount_history_entry, get_latest_amount_history
 
 route = APIRouter()
 
@@ -11,39 +12,35 @@ class CashAmounts(BaseModel):
     net_income_cents: int
     current_balance_cents: int
 
-# Add net income and current balance to amount_history_table.
-@route.post("/cash-amounts", status_code=status.HTTP_204_NO_CONTENT)
-def add_cash_amounts(cash_amounts: CashAmounts):
+# Update net income and current balance in latest amount_history_table entry.
+@route.put("/latest-cash-amounts", status_code=status.HTTP_200_OK)
+def set_latest_cash_amounts(cash_amounts: CashAmounts):
     conn = sqlite3.connect(DATABASE_DIR_PATH + DATABASE_NAME_PATH)
     cursor = conn.cursor()
-    
-    add_cash_amounts_query = (
-        "INSERT INTO amount_history_table "
-        "(net_income_cents, current_balance_cents) "
-        "VALUES (?, ?)"
-    )
 
-    cursor.execute(
-        add_cash_amounts_query, 
-        (cash_amounts.net_income_cents, cash_amounts.current_balance_cents)
+    amount_history_entry = get_latest_amount_history(cursor)
+    amount_history_id = amount_history_entry[0]
+
+    set_amount_history_entry(
+        cursor, 
+        cash_amounts.net_income_cents, 
+        cash_amounts.current_balance_cents,
+        amount_history_id
     )
     conn.commit()
-
-    get_timestamp_query = "SELECT entry_timestamp FROM amount_history_table ORDER BY entry_id DESC LIMIT 1"
-    cursor.execute(get_timestamp_query)
-    timestamp = cursor.fetchone()[0]
-    datetime_str = datetime.fromtimestamp(timestamp).strftime("%d/%b/%Y %I:%M:%S%p")
-    
+    latest_entry = get_latest_amount_history(cursor)
     conn.close()
     
     print((
-        "Record current cash amounts to amount history:\n"
-        f"Datetime: {datetime_str}\n"
+        "Updated current cash amounts in amount history:\n"
         f"Net income cents: {cash_amounts.net_income_cents}\n"
         f"Current balance cents: {cash_amounts.current_balance_cents}"
     ))
 
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return {
+        "net_income_cents": latest_entry[2], 
+        "current_balance_cents": latest_entry[3]
+    }
 
 # Save the current net income and current balance to JSON config.
 @route.put("/current-cash-amounts", status_code=status.HTTP_204_NO_CONTENT)
