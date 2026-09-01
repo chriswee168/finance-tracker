3268570a192bd2c41e7a8ca4193cb082d200e959
Christopher Wee <chriswee168@gmail.com>
2026-09-01 22:57:40 +0800

    Set next reset time after cash amount updates
    Timestamp is updated along with cash amounts in the same path
    operation function. Fixes issue where net income does not refresh
    when reset date is updated.

--- backend/routers/timestamp.py
+++ backend/routers/timestamp.py
@@ -11,18 +11,7 @@ route = APIRouter()
 def get_timestamp():
     conn = sqlite3.connect(DATABASE_DIR_PATH + DATABASE_NAME_PATH)
     cursor = conn.cursor()
-
-    # Check if current timestamp has exceeded interval since previous one.
-    if exceeded_timestamp_interval(cursor):
-        # Create new amount_history_table entry.
-        latest_amount_entry = get_latest_amount_history(cursor)
-        latest_timestamp = latest_amount_entry[1]
-        new_timestamp = latest_timestamp + TIMESTAMP_INTERVAL_SECS
-        append_amount_history_entry(cursor, new_timestamp, latest_amount_entry[3])
-        conn.commit()
-
-    latest_timestamp = get_latest_timestamp(cursor)
-    conn.commit()
+    latest_timestamp = timestamp_update(cursor, conn)
     conn.close()
 
     return {"timestamp": latest_timestamp}
